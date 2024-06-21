5e06645c9f707c0ad52a1ff5954116c499aa2b57
iamkipruto <ikruruto@gmail.com>
2024-06-21 14:40:28 +0300

    feat🎨✨: Added rounded Buttons to ReadingList component and Created a grid and cards

---
+++ src/frontend/src/components/ReadingList.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Grid, Card, CardMedia, CardContent, Typography, Button } from '@mui/material';
+
+const ReadingList = ({ books, onRemove }) => {
+  return (
+    <Grid container spacing={3}>
+      {books.map((book, index) => (
+        <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+          <Card>
+            <CardMedia
+              component="img"
+              height="140"
+              image={book.coverPhotoURL}
+              alt={book.title}
+            />
+            <CardContent>
+              <Typography variant="h6" component="div">
+                {book.title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Author: {book.author}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Reading Level: {book.readingLevel}
+              </Typography>
+              <Button variant="contained" color="secondary" onClick={() => onRemove(book)}>
+                Remove
+              </Button>
+            </CardContent>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default ReadingList;
